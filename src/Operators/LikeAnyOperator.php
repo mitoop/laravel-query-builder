1887d3a14585945fa16808bb51e1d3e29b6c6419
mitoop <me@mitoop.com>
2025-07-24 12:29:17 +0800

    enhance: LikeAnyOperator improvements
    Signed-off-by: mitoop <me@mitoop.com>

--- src/Operators/LikeAnyOperator.php
+++ src/Operators/LikeAnyOperator.php
@@ -2,6 +2,7 @@
 
 namespace Mitoop\LaravelQueryBuilder\Operators;
 
+use Closure;
 use Illuminate\Database\Eloquent\Builder;
 use Mitoop\LaravelQueryBuilder\Contracts\OperatorInterface;
 
@@ -9,8 +10,24 @@ class LikeAnyOperator implements OperatorInterface
 {
     public function apply(Builder $builder, string $whereType, string $field, $value): void
     {
-        [$field, $value] = $value;
+        [$field, $columns] = $value;
 
-        $builder->{"{$whereType}Any"}($field, 'like', $value);
+        $builder->{"{$whereType}"}(function (Builder $query) use ($columns, $field) {
+            foreach ($columns as $key => $column) {
+                if (is_int($key)) {
+                    $query->orWhere($column, 'like', $field);
+                } elseif (is_array($column)) {
+                    $relation = $key;
+                    $query->orWhereHas($relation, function (Builder $q) use ($column, $field) {
+                        foreach ($column as $relField) {
+                            $q->orWhere($relField, 'like', $field);
+                        }
+                    });
+                } elseif ($column instanceof Closure) {
+                    $relation = $key;
+                    $query->orWhereHas($relation, $column);
+                }
+            }
+        });
     }
 }
